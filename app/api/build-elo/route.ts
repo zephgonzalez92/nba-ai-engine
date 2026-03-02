f0af4a82a2d8070fce6e1f60e8a760f760657913
zephgonzalez92 <zephgonzalez92@gmail.com>
2026-03-01 20:25:48 -0600

    Update route.ts

--- app/api/build-elo/route.ts
+++ app/api/build-elo/route.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
